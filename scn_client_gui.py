0cfdfda604df8d4211522b1aa08cfc5a3e775f99
Alex <devkral@web.de>
2014-10-30 21:11:10 +0100

    Revert "rename gtkbuilder file"
    This reverts commit 5015fd6f4e2af700ab79223fdd301d927547f81b.

--- scn_client_gui.py
+++ scn_client_gui.py
@@ -1109,7 +1109,7 @@ if __name__ == "__main__":
   client_thread.daemon = True
   client_thread.start()
 
-  scnGUI(cm,"guiscn.ui")
+  scnGUI(cm,"guiscn.glade")
   while run==True:
     Gtk.main_iteration_do(True)
   
